2559d1b187110971ce72c16367525a8b5e62c6f5
worldrulesdev <worldrulesdev@gmail.com>
2023-11-13 22:17:16 -0300

    adjust font version and popularjobs

--- app/_layout.js
+++ app/_layout.js
@@ -6,19 +6,19 @@ import * as SplashScreen from 'expo-splash-screen';
 SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-    const [fontLoaded] = useFonts({
-        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
-        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
-        DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
-    })
+    const [fontsLoaded] = useFonts({
+        DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+        DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+        DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+    });
 
     const onLayoutRootView = useCallback(async () => {
-        if (fontLoaded) {
+        if (fontsLoaded) {
             await SplashScreen.hideAsync();
         }
-    }, [fontLoaded])
+    }, [fontsLoaded])
 
-    if (!fontLoaded) return null;
+    if (!fontsLoaded) return null;
     return <Stack onLayout={onLayoutRootView} />;
 }
 
