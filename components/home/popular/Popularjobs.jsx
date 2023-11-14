a068296c598a2ec034501af0ea19b447ed65286d
worldrulesdev <worldrulesdev@gmail.com>
2023-11-13 22:25:44 -0300

    font error solved

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -28,7 +28,7 @@ const Popularjobs = () => {
           <Text>Something went wrong</Text>
         ) : (
           <FlatList
-            data={[1, 2, 3, 4]}
+            data={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
             renderItem={({ item }) => (
               <PopularJobCard
                 item={item}
