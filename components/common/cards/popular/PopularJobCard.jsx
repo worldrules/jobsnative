c2ce02d2bf1c03c61f1c6ec22f3e88b07a44d11d
worldrulesdev <worldrulesdev@gmail.com>
2023-11-14 01:32:59 -0300

    checklogo function impl

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -11,7 +11,11 @@ const PopularJobCard = ({ selectedJob, item, handleCardPress }) => {
     >
       <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
         <Image
-          source={{ uri: item.employer_logo }}
+          source={{
+            uri: checkImageURL(item?.employer_logo)
+              ? item.employer_logo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+          }}
           resizeMode='contain'
           style={styles.logoImage}
         />
