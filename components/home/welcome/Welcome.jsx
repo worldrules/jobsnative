98893ed2be371def18dea421f3a9443c8789c95b
worldrulesdev <worldrulesdev@gmail.com>
2023-11-13 21:39:52 -0300

    welcome work more

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -48,11 +48,18 @@ const Welcome = () => {
         <FlatList
           data={jobTypes}
           renderItem={({ item }) => (
-            <TouchableOpacity style={styles.tab(activeJobType, item)}>
-              <Text>{item}</Text>
+            <TouchableOpacity style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item)
+                router.push(`/search/${item}`)
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
             </TouchableOpacity>
           )}
-
+          keyExtractor={item => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
         />
       </View>
 
