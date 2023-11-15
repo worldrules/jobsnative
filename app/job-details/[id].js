edf6e1662ed030a7ea49d7ad3ae95235858afd14
worldrulesdev <worldrulesdev@gmail.com>
2023-11-14 22:45:24 -0300

    start details from job

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,8 +1,44 @@
-import React from 'react'
+import {
+    Text,
+    SafeAreaView,
+    ScrollView,
+    ActivityIndicator,
+    RefreshControl
+
+} from 'react-native'
+import { Stack, useGlobalSearchParams, useRouter, useSearchParams } from 'expo-router';
+import { useCallback, useState } from 'react';
+
+import { Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics } from '../../components';
+import { COLORS, SIZES, icons } from '../../constants'
+import useFetch from '../../hook/useFetch';
 
 const JobDetails = () => {
+    const params = useGlobalSearchParams();
+    const router = useRouter();
+
+    const { data, isLoading, error, refetch } = useFetch('job-details', {
+        job_id: params.id
+    })
+
+
     return (
-        <div>JobDetails</div>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite },
+                    headerShadowVisible: false,
+                    headerBackVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.left}
+                            dimension="60%"
+                            handlePress={() => router.back()}
+                        />
+                    )
+                }}
+            >
+            </Stack.Screen>
+        </SafeAreaView>
     )
 }
 
